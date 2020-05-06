2b11d98257bac24295b455b11160cba9379065f0
Ibrahim AHMED BACHA <fi_ahmed_bacha@esi.dz>
2020-05-06 10:01:09 +0100

    Updates

---
+++ precache-manifest.fde8bbd4e7562af3aeeb1ab25756717f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce976eeede7a39bc898d70a69fec07c6",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "01e70fa00e449e7d6f07",
+    "url": "/would-you-rather/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "3b5aaa15565305c3cf4f",
+    "url": "/would-you-rather/static/js/2.ee563c45.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/would-you-rather/static/js/2.ee563c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e70fa00e449e7d6f07",
+    "url": "/would-you-rather/static/js/main.eba31be7.chunk.js"
+  },
+  {
+    "revision": "3aeeffe7ee51f78600a4",
+    "url": "/would-you-rather/static/js/runtime-main.1de267fd.js"
+  }
+]);
